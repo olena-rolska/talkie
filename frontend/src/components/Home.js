330e6935d09070be85dc000a42645f2dcc341ee6
olena_rolskaya <manitskayaelena@gmail.com>
2019-07-07 16:31:50 +0300

    home page finished

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,35 +1,57 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Bg from '../images/bg.png';
 
 const useStyles = makeStyles(theme => ({
-  header: {
+  main: {
+    width: '100%',
     height: '100hv',
-    color: 'red',
-    display: 'flex',
-    justifyContent: 'flex-end',
-    marginRight: 50,
-    marginTop: 50,
-    alignSelf: 'flex-end'
+    padding: 0,
+    backgroundImage: `url(${Bg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'fixed',
+  },
+  block: {
+    width: '80%',
+    height: '100hv',
+    margin: 'auto',
+    textAlign: 'center',
+    backgroundColor: '#39523b',
+    
+  },
+  title: {
+    marginTop: '1em',
+    paddingTop: '1em',
+    marginBottom: '1em',
   },
   link: {
     textDecoration: 'none',
-  }
+    cursor: 'pointer',
+    fontSize: '3em',
+  },
+  button: {
+    margin: theme.spacing(10),
+    fontSize: '0.5em',
+  },
+  input: {
+    display: 'none',
+  },
 }))
 
 const Home = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.header}>
-      <h1>Welcome to Talkie</h1>
-      <h2>Before you gonna talk, you need to choose</h2>
-      <RouterLink href="#" variant="body2" to="/signup/">
-        Sign up
-      </RouterLink>
-      <RouterLink href="#" variant="body2" to="/signin/">
-        Sign in
-      </RouterLink>
+    <div className={classes.main}>
+      <section className={classes.block}>
+        <Typography variant="h1" component="h1" className={classes.title}>Welcome to Talkie</Typography>
+        <Typography variant="h4" component="h2" className={classes.text}>Before you're gonna talk, you need to choose</Typography>
+        <RouterLink href="#" variant="body2" to="/signup/" className={classes.link}><Button className={classes.button}>SIGN UP</Button></RouterLink>
+        <RouterLink href="#" variant="body2" to="/signin/" className={classes.link}><Button className={classes.button}>SIGN IN</Button></RouterLink>
+      </section>
     </div>
   )
 }
